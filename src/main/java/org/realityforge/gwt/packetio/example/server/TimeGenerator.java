edcadfd7276072e03021f906050c97adec83a746
Peter Donald <peter@realityforge.org>
2014-01-23 19:40:55 +1100

    Support the server-side component of long polling

--- src/main/java/org/realityforge/gwt/packetio/example/server/TimeGenerator.java
+++ src/main/java/org/realityforge/gwt/packetio/example/server/TimeGenerator.java
@@ -1,6 +1,9 @@
 package org.realityforge.gwt.packetio.example.server;
 
+import java.util.Collection;
 import java.util.Date;
+import java.util.Iterator;
+import javax.ws.rs.container.AsyncResponse;
 import javax.ws.rs.core.MediaType;
 import org.glassfish.jersey.media.sse.OutboundEvent;
 import org.glassfish.jersey.media.sse.SseBroadcaster;
@@ -9,19 +12,37 @@ final class TimeGenerator
   implements Runnable
 {
   private final SseBroadcaster _broadcaster;
+  private final Collection<AsyncResponse> _waiters;
 
-  TimeGenerator( final SseBroadcaster broadcaster )
+  TimeGenerator( final SseBroadcaster broadcaster,
+                 final Collection<AsyncResponse> waiters )
   {
     _broadcaster = broadcaster;
+    _waiters = waiters;
   }
 
   @Override
   public void run()
   {
+    final String message = "The time is " + new Date();
     final OutboundEvent.Builder b = new OutboundEvent.Builder();
     b.mediaType( MediaType.TEXT_PLAIN_TYPE );
-    b.data( String.class, "The time is " + new Date() );
+    b.data( String.class, message );
 
     _broadcaster.broadcast( b.build() );
+
+    synchronized ( _waiters )
+    {
+      final Iterator<AsyncResponse> iterator = _waiters.iterator();
+      while( iterator.hasNext() )
+      {
+        final AsyncResponse response = iterator.next();
+        iterator.remove();
+        if( !response.isCancelled() )
+        {
+          response.resume( message );
+        }
+      }
+    }
   }
 }
--- src/main/java/org/realityforge/gwt/packetio/example/server/TimeResource.java
+++ src/main/java/org/realityforge/gwt/packetio/example/server/TimeResource.java
@@ -1,5 +1,7 @@
 package org.realityforge.gwt.packetio.example.server;
 
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -8,6 +10,8 @@ import javax.ejb.Singleton;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import org.glassfish.jersey.media.sse.EventOutput;
 import org.glassfish.jersey.media.sse.SseBroadcaster;
 
@@ -16,20 +20,29 @@ import org.glassfish.jersey.media.sse.SseBroadcaster;
 public class TimeResource
 {
   private final SseBroadcaster _broadcaster = new SseBroadcaster();
+  private final Collection<AsyncResponse> _waiters = new ConcurrentLinkedQueue<>(  );
   private final ScheduledExecutorService _scheduledExecutor = Executors.newSingleThreadScheduledExecutor();
 
   @PostConstruct
   public void postConstruct()
   {
-    _scheduledExecutor.scheduleWithFixedDelay( new TimeGenerator( _broadcaster ), 0, 1, TimeUnit.SECONDS );
+    _scheduledExecutor.scheduleWithFixedDelay( new TimeGenerator( _broadcaster, _waiters ), 0, 1, TimeUnit.SECONDS );
   }
 
   @GET
-  @Produces("text/event-stream")
+  @Produces( "text/event-stream" )
   public EventOutput getMessages()
   {
     final EventOutput eventOutput = new EventOutput();
     _broadcaster.add( eventOutput );
     return eventOutput;
+  }
+
+  @Path("long_poll")
+  @GET
+  @Produces( "text/plain" )
+  public void hangUp( @Suspended AsyncResponse response )
+  {
+    _waiters.add( response );
   }
 }
